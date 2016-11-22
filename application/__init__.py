c2302c020c1ec2ecf078ca2a2ef3193494dd3652
Matt Murbach <mmurbach@uw.edu>
2016-11-21 18:48:44 -0800

    append app to  path to static folder

--- application/__init__.py
+++ application/__init__.py
@@ -1,6 +1,6 @@
 from flask import Flask
 
-application = Flask(__name__, template_folder='./templates', static_url_path='/static')
+application = Flask(__name__, template_folder='./templates', static_url_path='/impedance-application/static')
 application.config.from_object('config')
 application.debug=True
 
