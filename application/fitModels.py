6f37da9c8e8363b690e31bc7339a79a5f8066986
Matt Murbach <mmurbach@uw.edu>
2017-01-24 08:59:37 -0800

    update data and fix file uploads

--- application/fitModels.py
+++ application/fitModels.py
@@ -10,245 +10,11 @@ import pandas as pd
 from scipy.optimize import leastsq, minimize
 from scipy.interpolate import interp1d
 
-def fitP2D(data):
-    """ Fit physics-based model
-
-    Parameters
-    -----------------
-    data : list of tuples
-        list of tuples containing (frequency, real impedance, imaginary impedance)
-
-    Returns
-    ------------
-    fit : list of tuples
-        list of tuples (frequency, real impedance, imaginary impedance) containing the best fit
-
-    sorted_results : pandas DataFrame
-        sorted DataFrame with columns of ['run', 'scale', 'residual']
-
-    Notes
-    ---------
-
-
-    """
-
-    # take incoming list of tuples and create a dataframe
-    # sorted by descending frequency
-    exp_data = pd.DataFrame(data, columns=['f', 'real', 'imag'])
-    exp_data['mag'] = exp_data.apply(lambda x: np.sqrt(x[1]**2 + x[2]**2), axis=1)
-    exp_data['phase'] = exp_data.apply(lambda x: np.arctan2(x[2], x[1]), axis=1)
-    exp_data.sort_values(by='f', ascending=False, inplace=True)
-    exp_data.index = range(len(exp_data))
-
-    # read in all of the simulation results
-    Z = pd.read_pickle('application/static/data/20688-Z.pkl')
-
-    # find the frequencies that fall within the experimental data and create
-    # the DataFrame, to_fit, to store interpolated experimental data for fitting
-    min_f, max_f = min(exp_data['f']), max(exp_data['f'])
-    freq_mask = sorted([f for f in Z.columns if min_f <= f <= max_f], reverse=True)
-
-    to_fit = pd.DataFrame(index=freq_mask, columns=['mag', 'ph'])
-
-    # if the frequency isn't already within 1% of the simulation frequencies,
-    # quadratically interpolate the nearest four points in the magnitude and phase
-    for frequency in to_fit.index:
-        exact = exp_data[exp_data['f'].between(.99*frequency, 1.01*frequency)]
-        if not exact.empty:
-            to_fit.loc[frequency, 'mag'] = np.asscalar(exact['mag'])
-            to_fit.loc[frequency, 'ph'] = np.asscalar(exact['phase'])
-        else:
-            idx = np.argmin(np.abs(frequency - exp_data['f']))
-
-            x = exp_data['f'].iloc[idx-2:idx+3]
-            y_mag = exp_data['mag'].iloc[idx-2:idx+3]
-            y_phase = exp_data['phase'].iloc[idx-2:idx+3]
-
-            mag = interp1d(x, y_mag, kind='quadratic')
-            phase = interp1d(x, y_phase, kind='quadratic')
-
-            to_fit.loc[frequency, 'mag'] = mag(frequency)
-            to_fit.loc[frequency, 'ph'] = phase(frequency)
-
-    to_fit['real'] = to_fit.mag*(to_fit.ph.map(np.cos))
-    to_fit['imag'] = to_fit.mag*(to_fit.ph.map(np.sin))
-
-    # if the imaginary impedance crosses zeros, finds the crossover
-    # and includes it in to_fit as the model's 10**5 Hz entry
-    crossover = exp_data[exp_data['imag'] > 0]
-
-    if crossover.index.tolist():
-        index = crossover.index.tolist()[-1]
-
-        x = exp_data['imag'].loc[index-2:index+3]
-        y = exp_data['real'].loc[index-2:index+3]
-
-        hf = interp1d(x,y, kind='quadratic')
-
-        Zreal_hf = np.asscalar(hf(0))
-
-        to_fit.drop(to_fit[to_fit['ph'] > 0].index, inplace=True)
-
-        to_fit = pd.concat([pd.DataFrame(data={'mag': Zreal_hf, 'ph': 0.0, 'real': Zreal_hf, 'imag': 0.0},
-                                index=[1e5], columns=to_fit.columns), to_fit])
-
-
-    Z11_exp = np.array(to_fit.real.tolist()) + 1j*np.array(to_fit.imag.tolist())
-
-    def residual(scale, Z11_model, Z11_exp):
-        '''
-        Returns average distance of error between the model and experimental data
-        '''
-        return (1./len(Z11_model))*np.sqrt(sum((np.real(Z11_exp) - scale*np.real(Z11_model))**2 + (np.imag(Z11_exp) - scale*np.imag(Z11_model))**2))
-
-    Z_array = np.array(Z)
-    results_array = np.ndarray((len(Z_array), 3))
-
-    mask = [i for i, f in enumerate(Z.columns) if f in to_fit.index]
-
-    for run, __ in enumerate(Z_array):
-        res = minimize(residual, 10.0, args=(Z_array[run, mask], Z11_exp), tol=1e-5)
-
-        results_array[run,0] = run
-        results_array[run,1] = res.x # scale
-        results_array[run,2] = res.fun # residual
-
-    results = pd.DataFrame(results_array, columns=['run', 'scale', 'residual'])
-
-    sorted_results = results.sort_values(['residual'])
-    sorted_results['residual'] = sorted_results['residual'].map(lambda x: x*100/(to_fit['mag'].mean()))
-
-    best_fit = int(sorted_results['run'].iloc[0])
-    Z11_model = sorted_results['scale'].iloc[0]*Z.iloc[best_fit, mask]
-
-    fit = zip(Z11_model.index, Z11_model.map(np.real), Z11_model.map(np.imag))
-    return fit, sorted_results.iloc[0:100]
-
-def fitP2D_Rohmic(data):
-    """ Fit physics-based model using estimation of ohmic resistance
-
-    Parameters
-    -----------------
-    data : list of tuples
-        list of tuples containing (frequency, real impedance, imaginary impedance)
-
-    """
-
-    # take incoming list of tuples and create a dataframe
-    # sorted by descending frequency
-    exp_data = pd.DataFrame(data, columns=['f', 'real', 'imag'])
-    exp_data['mag'] = exp_data.apply(lambda x: np.sqrt(x[1]**2 + x[2]**2), axis=1)
-    exp_data['phase'] = exp_data.apply(lambda x: np.arctan2(x[2], x[1]), axis=1)
-    exp_data.sort_values(by='f', ascending=False, inplace=True)
-    exp_data.index = range(len(exp_data))
-
-    # read in all of the simulation results
-    Z = pd.read_pickle('application/static/data/20688-Z.pkl')
-
-    # find the frequencies that fall within the experimental data and create
-    # the DataFrame, to_fit, to store interpolated experimental data for fitting
-    min_f, max_f = min(exp_data['f']), max(exp_data['f'])
-    freq_mask = sorted([f for f in Z.columns if min_f <= f <= max_f], reverse=True)
-
-    to_fit = pd.DataFrame(index=freq_mask, columns=['mag', 'ph'])
-
-    parameters = pd.read_csv('application/static/data/model_runs-full.txt', nrows = len(Z))
-
-    def calc_hf(p):
-        kappaeff_sep = p['kappa_0[S/m]']*p['epsilon_sep[1]']**1.5
-        kappaeff_pos = p['kappa_0[S/m]']*p['epsilon_pos[1]']**1.5
-        kappaeff_neg = p['kappa_0[S/m]']*p['epsilon_neg[1]']**1.5
-
-        sigmaeff_pos = p['sigma_pos[S/m]']*(1-p['epsilon_pos[1]']-p['epsilon_f_pos[1]'])**1.5
-        sigmaeff_neg = p['sigma_neg[S/m]']*(1-p['epsilon_neg[1]']-p['epsilon_f_neg[1]'])**1.5
-
-
-        R_sep = p['l_sep[m]']/kappaeff_sep
-        R_pos = p['l_pos[m]']/(kappaeff_pos + sigmaeff_pos)
-        R_neg = p['l_pos[m]']/(kappaeff_neg + sigmaeff_neg)
-
-        R_ohmic = R_sep + R_pos + R_neg
-
-        return R_ohmic
-
-    predicted = parameters.apply(calc_hf, axis=1)
-
-    # if the frequency isn't already within 1% of the simulation frequencies,
-    # quadratically interpolate the nearest four points in the magnitude and phase
-    for frequency in to_fit.index:
-        exact = exp_data[exp_data['f'].between(.99*frequency, 1.01*frequency)]
-        if not exact.empty:
-            to_fit.loc[frequency, 'mag'] = np.asscalar(exact['mag'])
-            to_fit.loc[frequency, 'ph'] = np.asscalar(exact['phase'])
-        else:
-            idx = np.argmin(np.abs(frequency - exp_data['f']))
-
-            x = exp_data['f'].iloc[idx-2:idx+3]
-            y_mag = exp_data['mag'].iloc[idx-2:idx+3]
-            y_phase = exp_data['phase'].iloc[idx-2:idx+3]
-
-            mag = interp1d(x, y_mag, kind='quadratic')
-            phase = interp1d(x, y_phase, kind='quadratic')
-
-            to_fit.loc[frequency, 'mag'] = mag(frequency)
-            to_fit.loc[frequency, 'ph'] = phase(frequency)
-
-    to_fit['real'] = to_fit.mag*(to_fit.ph.map(np.cos))
-    to_fit['imag'] = to_fit.mag*(to_fit.ph.map(np.sin))
-
-    crossover = exp_data[exp_data['imag'] > 0]
-
-    if crossover.index.tolist():
-        index = crossover.index.tolist()[-1]
-
-        x = exp_data['imag'].loc[index-2:index+3]
-        y = exp_data['real'].loc[index-2:index+3]
-
-        hf = interp1d(x,y, kind='quadratic')
-
-        Zreal_hf = np.asscalar(hf(0))
-
-        to_fit.drop(to_fit[to_fit['ph'] > 0].index, inplace=True)
-
-        to_fit = pd.concat([pd.DataFrame(data={'mag': Zreal_hf, 'ph': 0.0, 'real': Zreal_hf, 'imag': 0.0},
-                                index=[1e5], columns=to_fit.columns), to_fit])
-
-    hf_real = Z.loc[:,1e5].map(np.real)
-
-    scale = Zreal_hf/predicted
-    scale.index = range(1,len(scale)+1)
-
-    Z11_exp = np.array(to_fit.real.tolist()) + 1j*np.array(to_fit.imag.tolist())
-
-    mask = [i for i, f in enumerate(Z.columns) if f in to_fit.index]
-
-    results_array = np.ndarray(shape=(len(Z),3))
-
-    for run, impedance in enumerate(Z.iloc[:,mask].values):
-        scaled = impedance*scale.values[run]
-
-        residual = 1./len(scaled)*np.sqrt(sum((np.real(Z11_exp) - np.real(scaled))**2 + (np.imag(Z11_exp) - np.imag(scaled))**2))
-
-        results_array[run,0] = run
-        results_array[run,1] = scale.values[run]
-        results_array[run,2] = residual
-
-    results = pd.DataFrame(results_array, columns=['run', 'scale', 'residual'])
-
-    sorted_results = results.sort_values(['residual'])
-    sorted_results['residual'] = sorted_results['residual'].map(lambda x: x*100/(to_fit['mag'].mean()))
-
-    best_fit_idx = int(sorted_results['run'].iloc[0])
-    Z11_model = sorted_results['scale'].iloc[0]*Z.iloc[best_fit_idx, Z.columns >= min(freq)]
-
-    fit = zip(Z11_model.index, Z11_model.map(np.real), Z11_model.map(np.imag))
-    return fit, sorted_results.iloc[0:100]
-
 def fitP2D_matchHF(data):
     """ Fit physics-based model by matching the hf intercept
 
     Parameters
-    -----------------
+    ----------
     data : list of tuples
         list of tuples containing (frequency, real impedance, imaginary impedance)
 
@@ -263,7 +29,7 @@ def fitP2D_matchHF(data):
     exp_data.index = range(len(exp_data))
 
     # read in all of the simulation results
-    Z = pd.read_pickle('application/static/data/20688-Z.pkl')
+    Z = pd.read_pickle('application/static/data/25227-Z.pkl')
 
     # find the frequencies that fall within the experimental data and create
     # the DataFrame, to_fit, to store interpolated experimental data for fitting
@@ -341,7 +107,7 @@ def fitP2D_matchHF(data):
     Z11_model = Z.iloc[best_fit_idx, Z.columns >= min(exp_data['f'])]/sorted_results['scale'].iloc[0]
 
     fit = zip(Z11_model.index, Z11_model.map(np.real), Z11_model.map(np.imag))
-    return fit, sorted_results.iloc[0:100]
+    return fit, sorted_results.iloc[0:200]
 
 def calculateChisquared(Zdata, Zmodel, sigma):
     """ Returns the (:math:`\\chi^2`) goodness of fit statistic
@@ -410,19 +176,6 @@ def fitEC(data, circuit, initial_guess):
     global circuit_string
 
     circuit_string = circuit
-
-    # f = np.array([run[0] for run in data])
-    # real = np.array([run[1] for run in data])
-    # imag = np.array([run[2] for run in data])
-
-    # # Calculate best guesses
-    # r1 = real[np.argmax(f)]
-    # r2 = real.mean() - r1
-    #
-    # c1 = 1/(f[f>1][np.argmax(np.abs(np.arctan2(imag[f>1],real[f>1])))]*r1)
-    #
-    # parameters = [r1, r2, initial_guess[2], initial_guess[3], c1, initial_guess[5]]
-
 
     freq = np.array([a for a,b,c in data])
     zr = np.array([b for a,b,c in data])
--- application/views.py
+++ application/views.py
@@ -1,7 +1,7 @@
 from __future__ import print_function
 from application import application
 from flask import render_template, request, jsonify
-from application.fitModels import fitEC, fitP2D, fitP2D_Rohmic, fitP2D_matchHF
+from application.fitModels import fitEC, fitP2D_matchHF
 import scipy
 import sys, os
 import pandas as pd
@@ -9,207 +9,55 @@ import numpy as np
 import json
 
 # main webpage
-@application.route('/', methods=['GET', 'POST'])
-@application.route('/index', methods=['GET', 'POST'])
+@application.route('/', methods=['GET'])
+@application.route('/index', methods=['GET'])
 def index():
     "Impedance Analyzer Main Page"
 
-    default_context = {'data': "", 'ec_parameters': "", 'ecFit': False, 'p2d_parameters': "", 'p2dFit': False, 'p2d_residuals': "", 'p2d_simulations': "", 'p2d_names': ""}
+    return render_template('index.html')
 
-    #### if POST request triggered by form-data button ####
-    if request.method == 'POST' and 'data' in request.files:
+@application.route('/getExampleData', methods=['GET'])
+def getExampleData():
+    """ Gets the data from the selected example
 
-        fit_equivalent_circuit = 'fitting-ec' in request.values
-        fit_p2d = 'fitting-p2d' in request.values
+    Parameters
+    ----------
+    filename : str
+        filename for selecting example
 
-        if fit_equivalent_circuit:
-            circuit_string = request.values["circuit_string"]
+    Returns
+    -------
+    data : jsonified data
+    """
 
-            p_string = [p for p in circuit_string if p not in 'ps(),-/']
-
-            p0 = []
-
-            for a, b in zip(p_string[::2], p_string[1::2]):
-                p0.append(float(request.values[str(a + b)]))
-
-        #### if POST request contains an uploaded file #####
-        if request.files['data'].filename != "":
-            # print(request.files['data'].filename, file=sys.stderr)
-            f = request.files['data']
-            contents = f.read()
-            uploaded_data = to_array(contents)
-
-            # check if equivalent circuit check box is checked
-            if fit_equivalent_circuit:
-                p_results, p_error, ecFit = fitCircuit(uploaded_data, circuit_string, p0)
-
-                ec_parameters = [{"name": u"R1",  "value": format(p_results[0], '.4f'), "sensitivity": format(p_error[0], '.4f')},
-                                                {"name": u"R2", "value": format(p_results[1], '.4f'), "sensitivity": format(p_error[1], '.4f')},
-                                                {"name":u"W1", "value": format(p_results[2], '.4f'), "sensitivity": format(p_error[2], '.4f')},
-                                                {"name": u"W2",  "value": format(p_results[3], '.4f'), "sensitivity": format(p_error[3], '.4f')},
-                                                {"name": u"CPE1", "value": format(p_results[4], '.4f'), "sensitivity": format(p_error[4], '.4f')},
-                                                {"name":u"CPE2", "value": format(p_results[5], '.4f'), "sensitivity": format(p_error[5], '.4f')}]
-            else:
-                ec_parameters = ""
-                ecFit = False
-
-            # check if p2d check box is checked
-            if fit_p2d:
-                p2dFit, sorted_results  = fitP2D(uploaded_data)
-
-                Z = pd.read_pickle('./application/static/data/20688-Z.pkl')
-                Z.index = range(len(Z))
-
-                Z = Z.loc[sorted_results['run'].map(int).values]
-                p2d_simulations = pd.DataFrame(columns=['run', 'freq', 'real', 'imag'])
-
-                p2d_simulations['real'] = Z.apply(lambda y: ','.join(y.map(lambda x: str(np.real(x))).values.tolist()), axis=1)
-                p2d_simulations['imag'] = Z.apply(lambda y: ','.join(y.map(lambda x: str(np.imag(x))).values.tolist()), axis=1)
-                p2d_simulations['freq'] = Z.apply(lambda y: ','.join(Z.columns.map(str)), axis=1)
-                p2d_simulations['run'] = Z.index
-
-                parameters=pd.read_csv('./application/static/data/model_runs-full.txt')
-                P = parameters.loc[sorted_results['run'].map(int).values]
-
-                p2d_simulations['param'] =P.apply(lambda y: ','.join(y.map(lambda x: str(x)).values.tolist()), axis=1)
-
-                p2d_simulations = p2d_simulations.values.tolist()
-
-                p2d_names = P.columns.values.tolist()
-
-                best_fit = sorted_results['run'].iloc[0]
-                param_Series = parameters.loc[best_fit]
-
-                p2d_residuals = sorted_results.values.tolist()
-
-                p2d_parameters = []
-                for i, parameter in enumerate(param_Series.index):
-                    p2d_parameters.append({'name': parameter.split('[')[0], "value": param_Series.iloc[i], "sensitivity": "x"})
-            else:
-                p2d_parameters = ""
-                p2dFit = False
-                p2d_residuals = ""
-                p2d_simulations = ""
-                p2d_names = ""
-
-            context = {'data': example_data, 'ec_parameters': ec_parameters, 'ecFit': ecFit, 'p2d_parameters': p2d_parameters, 'p2dFit': p2dFit, 'p2d_residuals': p2d_residuals, 'p2d_simulations': p2d_simulations, 'p2d_names': p2d_names}
-
-            return render_template('index.html', **context)
-
-        #### else if POST request contains a selection from the example dropdown ####
-        elif request.values['example'] != "null":
-
-            # get data from POST request
-            filename = request.values['example']
-            with open('./application/static/data/examples/' + filename, 'r') as f:
-                contents = f.read()
-            example_data = to_array(contents)
-
-            # check if equivalent circuit check box is checked
-            if fit_equivalent_circuit:
-                p_results, p_error, ecFit = fitCircuit(example_data, circuit_string, p0)
-
-                parameter_units = request.values["parameter_units"].split(",")
-
-                ec_parameters = []
-
-                p_string = [p for p in circuit_string if p not in 'ps(),-/']
-
-                for i, (a, b) in enumerate(zip(p_string[::2], p_string[1::2])):
-                    ec_parameters.append({"name": str(a+b),
-                                                        "units": parameter_units[i],
-                                                        "value": format(p_results[i], '.4f'),
-                                                        "sensitivity": format(p_error[i], '.4f'),
-                                                        "percent_error": format(100*p_error[i]/p_results[i], '.2f')})
-
-            else:
-                ec_parameters = ""
-                ecFit = False
-
-            # check if p2d check box is checked
-            if fit_p2d:
-                p2dFit, sorted_results  = fitP2D_matchHF(example_data)
-
-                Z = pd.read_pickle('./application/static/data/20688-Z.pkl')
-                Z.index = range(len(Z))
-
-                mask = [f for f, r, i in p2dFit]
-
-                freq = [f for f, r, i in example_data]
-
-                Z = Z.loc[sorted_results['run'].map(int).values, Z.columns >= min(freq)]
-                p2d_simulations = pd.DataFrame(columns=['run', 'freq', 'real', 'imag'])
-
-                p2d_simulations['real'] = Z.apply(lambda y: ','.join(y.map(lambda x: str(np.real(x))).values.tolist()), axis=1)
-                p2d_simulations['imag'] = Z.apply(lambda y: ','.join(y.map(lambda x: str(np.imag(x))).values.tolist()), axis=1)
-                p2d_simulations['freq'] = Z.apply(lambda y: ','.join(Z.columns.map(str)), axis=1)
-                p2d_simulations['run'] = Z.index
-
-                parameters=pd.read_csv('./application/static/data/model_runs-full.txt')
-                P = parameters.loc[sorted_results['run'].map(int).values]
-
-                p2d_simulations['param'] = P.apply(lambda y: str(sorted_results['scale'].loc[int(y['run'])]*1e4) + ',' + ','.join(y.map(lambda x: str(x)).values.tolist()), axis=1)
-
-                p2d_simulations = p2d_simulations.values.tolist()
-
-                print(sorted_results, file=sys.stderr)
-
-                p2d_names = ['fit parameter[cm^2]'] + (P.columns.values.tolist())
-
-                best_fit = sorted_results['run'].iloc[0]
-                param_Series = parameters.loc[best_fit]
-
-                p2d_residuals = sorted_results.values.tolist()
-
-                p2d_parameters = []
-                p2d_parameters.append({"name": "fit parameter", "units": "cm^2",
-                                        "value": sorted_results['scale'].iloc[0]*1e4, "sensitivity": "x"})
-
-                for i, parameter in enumerate(param_Series.index):
-                    p2d_parameters.append({"name": parameter.split('[')[0], "units": parameter.split('[')[-1].strip("]"),
-                                                            "value": param_Series.iloc[i], "sensitivity": "x"})
-            else:
-                p2d_parameters = ""
-                p2dFit = False
-                p2d_residuals = ""
-                p2d_simulations = ""
-                p2d_names = ""
-
-            context = {'data': example_data, 'ec_parameters': ec_parameters, 'ecFit': ecFit, 'p2d_parameters': p2d_parameters, 'p2dFit': p2dFit, 'p2d_residuals': p2d_residuals, 'p2d_simulations': p2d_simulations, 'p2d_names': p2d_names}
-
-            return render_template('index.html', **context)
-
-    #### initial load + load after "remove file" button ####
-    return render_template('index.html', **default_context)
-
-@application.route('/getData', methods=['GET'])
-def getData():
-    data_type = request.values["data_type"]
     filename = request.values["filename"]
 
-    if data_type == "example":
-        with open('./application/static/data/examples/' + filename, 'r') as f:
-            contents = f.read()
-    elif data_type == "upload":
-        f = request.files['data']
+    with open('./application/static/data/examples/' + filename, 'r') as f:
         contents = f.read()
 
     data = to_array(contents)
 
     return jsonify(data=data)
 
+@application.route('/getUploadData', methods=['POST'])
+def getUploadData():
+    f = request.files['data']
+    contents = f.read()
+    data = to_array(contents)
+
+    return jsonify(data=data)
+
 @application.route('/fitCircuit', methods=['POST'])
 def fitCircuit():
+    data = request.values["data"].split(',')
+    f = [float(f) for f in data[::3]]
+    real = [float(r) for r in data[1::3]]
+    imag = [float(i) for i in data[2::3]]
+    data = zip(f,real,imag)
+
     circuit = request.values["circuit"]
-    example = request.values["example"]
     p0 = request.values["p0"]
     p0 = [float(p) for p in p0.split(',')]
-
-    if example:
-        filename = request.values["filename"]
-        with open('./application/static/data/examples/' + filename, 'r') as f:
-            contents = f.read()
-        data = to_array(contents)
 
     p_results, p_error, ecFit = fitEC(data, circuit, p0)
 
@@ -218,17 +66,15 @@ def fitCircuit():
 
 @application.route('/fitPhysics', methods=['POST'])
 def fitPhysics():
-    example = request.values["example"]
-
-    if example:
-        filename = request.values["filename"]
-        with open('./application/static/data/examples/' + filename, 'r') as f:
-            contents = f.read()
-        data = to_array(contents)
+    data = request.values["data"].split(',')
+    f = [float(f) for f in data[::3]]
+    real = [float(r) for r in data[1::3]]
+    imag = [float(i) for i in data[2::3]]
+    data = zip(f,real,imag)
 
     p2dFit, sorted_results  = fitP2D_matchHF(data)
 
-    Z = pd.read_pickle('./application/static/data/20688-Z.pkl')
+    Z = pd.read_pickle('application/static/data/25227-Z.pkl')
     Z.index = range(len(Z))
 
     mask = [f for f, r, i in p2dFit]
@@ -280,10 +126,3 @@ def to_array(input):
     col2 = [float(x) for x in input.split(',')[2:-1:3]]
 
     return zip(col0, col1,col2)
-
-# def jsonify(names, values):
-#     d = {}
-#     for name, value in zip(names, values):
-#         d[name] = value
-
-    # return json.dumps(d)
