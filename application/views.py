0c5a5dc326b7a75e727c2b7fe41c701313280198
Matt Murbach <mmurbach@uw.edu>
2017-04-13 22:01:26 -0700

    update version of static files

--- application/views.py
+++ application/views.py
@@ -12,7 +12,7 @@ from application.ECfit import fitEC
 @application.route('/', methods=['GET'])
 def index():
     """ Impedance Analyzer Main Page """
-    return render_template('index.html', version='20171304v2')
+    return render_template('index.html', version='20171304v3')
 
 
 @application.route('/getExampleData', methods=['GET'])
